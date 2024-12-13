ca83382ceb98aabc58748a3df1e56c22ec3a1515
DeathLines <deathlines11@gmail.com>
2024-12-13 18:41:45 +0300

    Servise kadar tamam

--- main.py
+++ main.py
@@ -5,80 +5,98 @@ from watchdog.events import FileSystemEventHandler, LoggingEventHandler
 import os
 import datetime
 
-# Bilgisayarın yerel tarih ve saatini al
-now = datetime.datetime.now()
 
-# Gün, ay, yıl ve saati belirli bir formatta yazdır
-formatted_time = now.strftime("%d-%m-%Y %H:%M:%S")
-
-
-
+id = 0
 
 dic = {
-    "FilePath" : None,
-    "Date" : now.strftime("%d-%m-%Y %H:%M:%S"),
-    "Event" : None
+    "ID": None,
+    "Olay": None,
+    "Tarih": None,
+    "Dosya_Yolu": None
 }
+diclist = []
 
-def changer(type,path):
+def changer(type):
     print("changer")
     if type == "1":
-        dic["Event"] = "Dosya oluşturuldu."
-        dic["FilePath"] = path
+        dic["Olay"] = "Dosya oluşturuldu."
     elif type == "2":
-        dic["Event"] = "Dosya değiştirildi."
-        dic["FilePath"] = path
+        dic["Olay"] = "Dosya değiştirildi."
     elif type == "3":
-        dic["Event"] = "Dosya silindi."
-        dic["FilePath"] = path
+        dic["Olay"] = "Dosya silindi."
     elif type == "4":
-        dic["Event"] = "Dosya taşındı"
-        dic["FilePath"] = path
+        dic["Olay"] = "Dosya taşındı"
 
 
-def jsoned():
-    with open("logfile.log","w+",encoding="utf-8") as file:
-        jstring = json.dumps(dic)
-        print("jstring: " +jstring)
-        file.writelines(jstring)
+def jsoned(path):
+    global id
+
+    now = datetime.datetime.now()
+    dic_copy = dic.copy()  
+    dic_copy["ID"] = id  
+    dic_copy["Tarih"] = now.strftime("%d-%m-%Y %H:%M:%S") + "." + str(now.microsecond)[:3]
+    dic_copy["Dosya_Yolu"] = path
+    id += 1  
+    diclist.append(dic_copy)  
+    jstring = json.dumps(diclist, ensure_ascii=False)
+
+    with open(r"D:\Yazilim\Python\PythonWritten\BilisimAltyapi\logfile.json", "w", encoding="utf-8") as file:
+        file.write(jstring)
+    
+    #silinebilir
+    for i in diclist:
+        print(i)
 
 
 class MyHandler(LoggingEventHandler):
+    def __init__(self):
+        self.last_modified_time = datetime.datetime.now()  
+
     def on_created(self, event):
-        print("created")
-        changer("1",event.src_path)
-        jsoned()
+        print("\n\n\nDosya Oluşturuldu\n\n\n")
+        changer("1")
+        jsoned(event.src_path)
 
     def on_modified(self, event):
-        changer("2",event.src_path)
-        jsoned()
+
+        time_diff = datetime.datetime.now() - self.last_modified_time
+        if time_diff.total_seconds() < 1:  
+            return
+        print("\n\n\nDosya Değiştirildi\n\n\n")
+        changer("2")
+        jsoned(event.src_path)
+        self.last_modified_time = datetime.datetime.now()  
 
     def on_deleted(self, event):
-        changer("3",event.src_path)
-        jsoned()
+        print("\n\n\nDosya Silindi\n\n\n")
+        changer("3")
+        jsoned(event.src_path)
 
     def on_moved(self, event):
-        changer("4",event.src_path)
-        jsoned()
+        print("\n\n\nDosya Taşındı\n\n\n")
+        changer("4")
+        jsoned(event.src_path)
+        self.last_modified_time = datetime.datetime.now()  
 
 
 if __name__ == "__main__":
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'  
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = "Dosya"  # Silinecek
 
     if not os.path.exists(path):
         print(f"Error: The path {path} does not exist.")
         sys.exit(1)
 
-    event_handler = MyHandler()  
+    event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
-
 
     try:
         print(f"Observing changes in: {path}")
         while observer.is_alive():
             print("observer")
+            print("Len: ", len(diclist))
             observer.join(1)
     except KeyboardInterrupt:
         print("Process interrupted.")
